29cc0efe43af64c7715f5ab5750a2ffdf5528bbb
4861727279204c6565 <101452696+harryleevn93@users.noreply.github.com>
2023-02-01 13:26:44 +0700

    Responsive Website Added on CSS
    Also Back to Top Button is working. First extra Js ^^!

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+// Back to Top Scripts
+
+// Get the button:
+let top_button = document.getElementById("top-Btn");
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
